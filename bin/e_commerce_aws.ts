d5cc7833b92014b9cf3e6f1c7024c7ba05133b0d
Victor Hugo Ramos <victorhramos.ti@outlook.com>
2026-05-20 15:46:51 -0300

    feat: estrutura inicial do projeto e infraestrutura cdk

--- bin/e_commerce_aws.ts
+++ bin/e_commerce_aws.ts
@@ -1,20 +1,38 @@
 #!/usr/bin/env node
-import * as cdk from 'aws-cdk-lib/core';
-import { ECommerceAwsStack } from '../lib/e_commerce_aws-stack';
+import 'source-map-support/register'
+import * as cdk from 'aws-cdk-lib';
+import { ProductsAppStack } from '../lib/productsApp-stack';
+import { ECommerceApiStack } from '../lib/ecommerceApi-stack';
+import { ProductsAppLayerStack } from '../lib/productAppLayers-stack';
 
 const app = new cdk.App();
-new ECommerceAwsStack(app, 'ECommerceAwsStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
 
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+const env: cdk.Environment = {
+  account: "878670310640",
+  region: "us-east-1"
+}
 
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
+const tags = {
+  cost: "ECommerce",
+  team: "Myteam"
+}
 
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
-});
+const productsAppLayerStack = new ProductsAppLayerStack(app, "ProductsAppLayers", {
+  tags: tags,
+  env: env
+})
+
+const productsAppStack = new ProductsAppStack(app, "ProductsApp", {
+  tags: tags,
+  env: env
+})
+
+productsAppStack.addDependency(productsAppLayerStack)
+
+const eCommerceApiStack = new ECommerceApiStack(app, "ECommerceApi", {
+  productsFetchHandler: productsAppStack.productsFetchHandler,
+  productsAdminHandler: productsAppStack.productsAdminHandler,
+  tags: tags,
+  env: env
+})
+eCommerceApiStack.addDependency(productsAppStack)
